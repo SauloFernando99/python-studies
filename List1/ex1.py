7e43c5c527351ff9ac48cf4dbfbc4342f21973e7
SauloFernando99 <saulofernando114@gmail.com>
2024-02-22 20:31:20 -0300

    feat: solution for the exercise number 1

---
+++ List1/ex1.py
@@ -0,0 +1,9 @@
+#Find the biggest number between 2 numbers
+
+number1 = int(input("First number: "))
+number2 = int(input("Second number: "))
+
+if(number1 > number2):
+    print(f"First number: {number1}, is the biggest")
+else:
+    print(f"Second number: {number2}, is the biggest")
