cdafb80d00104cdcce04cd57969076512ebe5926
SauloFernando99 <saulofernando114@gmail.com>
2024-02-23 14:30:05 -0300

    refactor: adjustment to make the code more effective

--- List1/Exercise2/ex2.py
+++ List1/Exercise2/ex2.py
@@ -6,7 +6,7 @@ number3 = int(input("Third number: "))
 
 if (number1 > number2 and number1 > number3):
     print(f"First number: {number1}, is the biggest")
-if (number2 > number1 and number2 > number3):
+elif (number2 > number1 and number2 > number3):
     print(f"Second number: {number2}, is the biggest")
-if (number3 > number1 and number3 > number2):
+elif (number3 > number1 and number3 > number2):
      print(f"Third number: {number3}, is the biggest")
