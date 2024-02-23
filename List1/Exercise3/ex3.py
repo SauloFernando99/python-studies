2ae613e3d052ca12ae80a41ecdc39fc8c0645d39
SauloFernando99 <saulofernando114@gmail.com>
2024-02-23 14:33:23 -0300

    refactor: adjustment to make the code more effective

--- List1/Exercise3/ex3.py
+++ List1/Exercise3/ex3.py
@@ -1,13 +1,9 @@
-#Find how many numbers between a list of 5 number are bigger than 10
+#Read 5 numbers and count how many of then are bigger than 10
 
-numbers = []
 count = 0
 
 for i in range(5):
     num = int(input(f"number {i+1}: "))
-    numbers.append(num)
-
-for num in numbers:
     if num > 10:
         count += 1
 
