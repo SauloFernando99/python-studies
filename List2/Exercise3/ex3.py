79c16dda4361fada0c8e451f059cea34982c96eb
SauloFernando99 <saulofernando114@gmail.com>
2024-02-28 19:53:57 -0300

    feat: solution for exercise 3

---
+++ List2/Exercise3/ex3.py
@@ -0,0 +1,6 @@
+# Number power
+
+base = float(input("insert a number for base: "))
+exponent = float(input("insert a number for exponent: "))
+
+print(f"{base} elevated by {exponent} = {base ** exponent}")
