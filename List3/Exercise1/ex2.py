04759f906c9516fcdf6a9f36fc0ab05c6a55e356
SauloFernando99 <saulofernando114@gmail.com>
2024-05-29 09:33:56 -0300

    feat: new function to calculate the mean age of the players whose plays in corinthians and have weight above 80 kilos

--- List3/Exercise1/ex2.py
+++ List3/Exercise1/ex2.py
@@ -16,8 +16,24 @@ def registry_players():
 
     return players
 
+
+def mean_age_from_corinthians_players_with_weight_above_80(players):
+    total_players = 0
+    age_sum = 0
+
+    for player in players:
+        if player[2] > 80 and player[3] == "corinthians":
+            total_players += 1
+            age_sum += player[1]
+
+    return age_sum/total_players
+
+
 def main():
     players = registry_players()
+
+    print(f"Mean age of players from Corinthians and weight above 80 kilos: "
+          f"{mean_age_from_corinthians_players_with_weight_above_80(players)}")
 
 
 if __name__ == "__main__":
