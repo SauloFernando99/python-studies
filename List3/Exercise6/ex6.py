9991ec1ac35668c7f8ba23378c299ca736351adf
SauloFernando99 <saulofernando114@gmail.com>
2024-05-30 14:23:50 -0300

    feat: solution for exercise 6

---
+++ List3/Exercise6/ex6.py
@@ -0,0 +1,28 @@
+def is_prime_number(number):
+    if number < 2:
+        return "no"
+
+    for i in range(2, number):
+        if number % i == 0:
+            return "no"
+
+    return "yes"
+
+
+def verify_prime_numbers(number):
+
+    for i in range(2, number):
+        prime_number = is_prime_number(i)
+
+        if prime_number == "yes":
+            print(f"{i}")
+
+
+def main():
+    number = int(input("Insert number: "))
+
+    verify_prime_numbers(number)
+
+
+if __name__ == "__main__":
+    main()
